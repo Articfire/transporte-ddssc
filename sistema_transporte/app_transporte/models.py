aa8a34125518e85fc148c0ef2bea94af5b589e70
Articfire <martindrumer21@gmail.com>
2020-04-02 17:46:50 -0700

    Modelo de checklist removido por que no es necesario, y migracion de cambios realizada.

--- sistema_transporte/app_transporte/models.py
+++ sistema_transporte/app_transporte/models.py
@@ -38,19 +38,13 @@ class Vehiculos(models.Model):
     def __str__(self):
         print(self.marca+" "+self.modelo)
 
-class Checklist(models.Model):
-    chofer   = models.OneToOneField(Empleados, on_delete=models.CASCADE)
-    vehiculo = models.OneToOneField(Vehiculos, on_delete=models.CASCADE)
-
-    def __str__(self):
-        print("Agenda de "+Empleados.objects.get(id=self.chofer)+", conductor del "+Vehiculos.objects.get(id=self.vehiculo))
-
 class Servicios(models.Model):
-    checklist     = models.ForeignKey(Checklist, on_delete=models.CASCADE)
     lugar_inicio  = models.CharField(max_length=50)
     lugar_destino = models.CharField(max_length=50)
     fecha_inicio  = models.DateTimeField()
     descripcion   = models.CharField(max_length=50)
+    chofer        = models.ForeignKey(Empleados, verbose_name="Chofer", on_delete=models.CASCADE)
+    vehiculo     = models.ForeignKey(Vehiculos, verbose_name="Vehiculo", on_delete=models.CASCADE)
 
     ESTADOS_DEL_SERVICIO = [
         ('PH', 'Por hacer'),
@@ -65,20 +59,3 @@ class Servicios(models.Model):
 
     def __str__(self):
         print(self.descripcion)
-
-
-# class LlavesLiberacion(models.Model):
-#     llave_liberacion = models.CharField(max_length=20)
-#     servicio = models.OneToOneField(Servicios, on_delete=models.CASCADE)
-
-
-# Ideas:
-# 1.Que haya una tabla llamada "LlavesLiberacion" que
-# contenga codigos alfanumericos que el gerente le da 
-# al cliente una vez que se registra su solicitud de 
-# transporte para que solo el cliente pueda liberar un 
-# servicio de transporte desde el sitio web, en el cual 
-# habra un campo de texto para que el cliente inserte 
-# el token desde su dispositivo.
-#
-# 2.
