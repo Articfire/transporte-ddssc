e4085c579c8dda804779d31a05dcde2005d7ab79
Articfire <martindrumer21@gmail.com>
2020-05-29 13:01:03 -0700

    Corregir los modelos y Corregir las interfaces graficas.

--- sistema_transporte/app_transporte/models.py
+++ sistema_transporte/app_transporte/models.py
@@ -3,66 +3,64 @@ from django.utils import timezone
 
 
 class Empleados(models.Model):
-    nombres          = models.CharField(max_length=20)
+    nombres = models.CharField(max_length=40)
     apellido_materno = models.CharField(max_length=20)
     apellido_paterno = models.CharField(max_length=20)
     fecha_nacimiento = models.DateField()
-    rfc              = models.CharField(max_length=13)
+    rfc = models.CharField(max_length=13)
 
-    PUESTOS_DISPONIBLES = [
-        ('CH', 'Chofer'),
-        ('TM', 'Tecnico de mantenimiento'),
-        ('SC', 'Supervisor de Chofer'),
-        ('JM', 'Jefe de mantenimiento'),
-        ('GT', 'Gerente de transporte')]
-    puesto              = models.CharField(
+    puesto = models.CharField(
         max_length=2,
-        choices=PUESTOS_DISPONIBLES,
-        default='CH')
+        choices=[
+			('CH', 'Chofer'),
+			('TM', 'Tecnico de mantenimiento'),
+			('SC', 'Supervisor de Chofer'),
+			('JM', 'Jefe de mantenimiento'),
+			('GT', 'Gerente de transporte')
+		],
+        default='CH'
+	)
 
 class Vehiculos(models.Model):
-    marca  = models.CharField(max_length=20)
+    marca = models.CharField(max_length=20)
     modelo = models.CharField(max_length=20)
-
-    ESTADOS_DEl_VEHICULO = [
-        ('DI', 'Disponible'),
-        ('NO', 'No Disponible')]
-    estado = models.CharField(
-        max_length=2,
-        choices=ESTADOS_DEl_VEHICULO,
-        default='DI')
+    disponible = models.BooleanField()
 
 class Clientes(models.Model):
-    nombres          = models.CharField(max_length=20)
+    nombres = models.CharField(max_length=20)
     apellido_materno = models.CharField(max_length=20)
     apellido_paterno = models.CharField(max_length=20)
-    telefono         = models.CharField(max_length=20)
-    correo           = models.CharField(max_length=30)
-    clave            = models.CharField(max_length=20)
+    telefono = models.CharField(max_length=20)
+    correo = models.CharField(max_length=30)
 
 class Pagos(models.Model):
-    nombre_titular   = models.CharField(max_length=20)
-    numero_tarjeta   = models.CharField(max_length=40)
+    nombre_titular = models.CharField(max_length=20)
+    numero_tarjeta = models.CharField(max_length=20)
     fecha_expiracion = models.DateField(auto_now=False, auto_now_add=False)
     codigo_seguridad = models.IntegerField()
 
 class Servicios(models.Model):
-    lugar_inicio  = models.CharField(max_length=50)
-    lugar_destino = models.CharField(max_length=50)
-    fecha         = models.DateTimeField()
-    a_transportar = models.CharField(max_length=50)
-    chofer        = models.ForeignKey(Empleados, on_delete=models.CASCADE)
-    vehiculo      = models.ForeignKey(Vehiculos, on_delete=models.CASCADE)
-    solicitante   = models.OneToOneField(Clientes, on_delete=models.CASCADE)
-    pago          = models.OneToOneField(Pagos, on_delete=models.CASCADE)
+	chofer = models.ForeignKey(Empleados, on_delete=models.CASCADE)
+	vehiculo = models.ForeignKey(Vehiculos, on_delete=models.CASCADE)
+	cliente = models.OneToOneField(Clientes, on_delete=models.CASCADE)
+	pago = models.OneToOneField(Pagos, on_delete=models.CASCADE)
+	mascota = models.CharField(max_length=20)
+	lugar_inicio = models.CharField(max_length=40)
+	lugar_destino = models.CharField(max_length=40)
 
-    ESTADOS_DEL_SERVICIO = [
-        ('SS', 'Servicio Solicitado')
-        ('PH', 'Por hacer'),
-        ('EP', 'En proceso'),
-        ('TE', 'Terminado'),
-        ('FI', 'Firmado'),]
-    estado        = models.CharField(
-        choices=ESTADOS_DEL_SERVICIO, 
-        max_length=2, 
-        default='PH')
+	estado = models.CharField(
+		max_length=2,
+		choices=[
+			('SO', 'Solicitado'),
+			('AG', 'Agendado'),
+			('EJ', 'Ejecucion'),
+			('TE', 'Terminado'),
+			('FI', 'Firmado')
+		],
+		default='SO'
+	)
+
+class Agenda(models.Model):
+	servicio = models.OneToOneField(Servicios, on_delete=models.CASCADE)
+	fecha_inicio = models.DateTimeField()
+	fecha_limite = models.DateTimeField()
